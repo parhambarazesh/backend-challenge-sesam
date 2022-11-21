9e3b052f3c96cd33e9288272a40c642bd8d5d9aa
parhambarazesh <parham.barazesh@gmail.com>
2022-11-21 20:08:26 +0100

    add create dataset to create and save dataset to a file

---
+++ src/server/create_dataset.py
@@ -0,0 +1,22 @@
+import json
+import os
+import os.path as path
+from uuid import uuid4
+
+
+def create_and_save_dataset_as_file(json_list):
+    try:
+        dataset_id=str(uuid4())
+        dataset_path=path.dirname(path.dirname(path.abspath(__file__)))+os.sep+"uploaded_datasets"+os.sep+dataset_id
+        if not path.exists(dataset_path):
+            os.makedirs(dataset_path)
+        object_id_list=[]
+        for dict_data in json_list:
+            object_id=str(uuid4())
+            file_name=dataset_path+os.sep+object_id+".json"
+            with open(file_name, 'w') as outfile:
+                json.dump(dict_data, outfile, indent=4)
+            object_id_list.append(object_id)
+        return {'status': 'Dataset successfully created', 'dataset_id':dataset_id, 'object_ids':object_id_list,'Code':200}
+    except Exception as e:
+        return {'status': 'Dataset Not Created', 'dataset_info':[],'Code':404, 'error':str(e)}
