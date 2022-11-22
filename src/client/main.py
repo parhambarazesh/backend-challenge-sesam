0882e0ad97d6e2b6f12f351a270f27a9c1cea02a
parhambarazesh <parham.barazesh@gmail.com>
2022-11-23 00:21:50 +0100

    send http requests in the client over https

--- src/client/main.py
+++ src/client/main.py
@@ -1,7 +1,14 @@
+import os
 import click
 import requests
 import ast
 from src.client.settings import *
+from src.client.secrets import *
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
+
+def get_cert():
+    return os.path.dirname(os.path.abspath(__file__))+os.sep+"secrets"+os.sep+"cert.pem"
 
 @click.group()
 def all_commands():
@@ -13,36 +20,36 @@ def postdataset(datasetfile):
     if datasetfile:
         with open(datasetfile, 'r') as f:
             dataset = f.read()
-        response=requests.post(url, json=ast.literal_eval(dataset))
+        response=requests.post(url, json=ast.literal_eval(dataset), verify=get_cert())
         click.echo(response.json())
 
 @click.command()
 @click.option('--all', is_flag=True)
 @click.option('--id', type=str)
-def getdatasets(all,id):
+def getdataset(all,id):
     if all:
-        response=requests.get(url)
+        response=requests.get(url,verify=get_cert())
         click.echo(response.json())
     elif id:
-        response=requests.get(url+"/"+id)
+        response=requests.get(url+"/"+id,verify=get_cert())
         click.echo(response.json())
 
 @click.command()
 @click.option('--id', type=str)
 def deletedataset(id):
     if id:
-        response=requests.delete(url+"/"+id)
+        response=requests.delete(url+"/"+id,verify=get_cert())
         click.echo(response.json())
 
 @click.command()
 @click.option('--id', type=str)
 def exportdataset(id):
     if id:
-        response=requests.get(url+"/"+id+'/excel')
+        response=requests.get(url+"/"+id+'/excel',verify=get_cert())
         click.echo(response.json())
 
 
-all_commands.add_command(getdatasets)
+all_commands.add_command(getdataset)
 all_commands.add_command(postdataset)
 all_commands.add_command(deletedataset)
 all_commands.add_command(exportdataset)
