40d6f59444901270da930aa9f38e77278c35824e
parhambarazesh <parham.barazesh@gmail.com>
2022-11-23 00:19:46 +0100

    add db connector for mongodb

--- src/server/create_dataset.py
+++ src/server/create_dataset.py
@@ -3,6 +3,8 @@ import os
 import os.path as path
 from uuid import uuid4
 
+from src.db_connector.mongodb import store_dataset
+
 def create_and_save_dataset_as_file(json_list):
     try:
         dataset_id=str(uuid4())
@@ -15,6 +17,7 @@ def create_and_save_dataset_as_file(json_list):
             file_name=dataset_path+os.sep+object_id+".json"
             with open(file_name, 'w') as outfile:
                 json.dump(dict_data, outfile, indent=4)
+                store_dataset(dataset_id,dict_data)
             object_id_list.append(object_id)
         return {'status': 'Dataset successfully created', 'dataset_id':dataset_id, 'object_ids':object_id_list,'Code':200}
     except Exception as e:
--- src/server/delete_dataset.py
+++ src/server/delete_dataset.py
@@ -2,11 +2,13 @@ import json
 import os
 import os.path as path
 import shutil
+from src.db_connector.mongodb import delete_dataset
 
 def delete_dataset_by_id_from_file(id):
     dataset_path=path.dirname(path.dirname(path.abspath(__file__)))+os.sep+"uploaded_datasets"+os.sep+id
     if path.exists(dataset_path):
         shutil.rmtree(dataset_path)
+        delete_dataset(id)
         return {'status': 'Dataset successfully deleted','dataset_id':id,'Code':200}
     else:
         return {'status': 'Dataset Not Found','dataset_id':id,'Code':404}
