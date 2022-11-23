f53a685d8a54156a71cb528f921d9c63ddfc2888
parhambarazesh <parham.barazesh@gmail.com>
2022-11-23 01:38:18 +0100

    fix specs

--- src/tests/server.tests/test_createdataset.py
+++ src/tests/server.tests/test_createdataset.py
@@ -10,7 +10,7 @@ def test_createdatasetasfile_whendatasetnotfound_returns404(mocker1, mocker2):
 @mock.patch('src.server.create_dataset.os.path.exists', return_value=True)
 @mock.patch('src.server.create_dataset.json.dump', return_value=True)
 @mock.patch("builtins.open", new_callable=mock.mock_open)
-@mock.patch('src.server.create_dataset.write_into_database', return_value=True)
+@mock.patch('src.server.create_dataset.store_dataset', return_value=True)
 def test_createdatasetasfile_whendatasetfound_returns200(mocker1, mocker2, mocker3, mocker4):
     response = create_and_save_dataset_as_file([{'a': 1, 'b': 2}, {'c': 3, 'd': 4}])
     assert response['Code'] == 200
